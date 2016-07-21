acdd065c432550453537533c63e349518da453bd
Joshua Ugba <justjosh001@gmail.com>
2016-07-21 23:35:29 +0100

    Add flask_script for cli options

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Hello World and your broswer is %s</h1>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    manager.run()
