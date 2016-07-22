905da1301940d489ede8855e83bf6aeda9557b50
Joshua Ugba <justjosh001@gmail.com>
2016-07-22 07:09:03 +0100

    Add templates and bootstrap

--- hello.py
+++ hello.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello World and your broswer is %s</h1>' % user_agent
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!</h1>' % name
+    return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html')
 
 if __name__ == '__main__':
     manager.run()
