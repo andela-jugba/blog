bf6f8383c1c488e6b7c65bf510b05a50f407f458
Joshua Ugba <justjosh001@gmail.com>
2016-07-23 18:15:25 +0100

    Add db connections

--- hello.py
+++ hello.py
@@ -1,19 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, session, flash
 from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'You will never guess what is in here'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 momemt = Moment(app)
 
-@app.route('/')
+
+class NameForm(Form):
+    name = StringField('What is your name', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(64), unique=True)
+    users= db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you changed your name')
+            session['name']= form.name.data
+        else:
+            flash('Your name seems to remain the s')
+        form.name.data=''
+        return redirect(url_for('index'))
+
     user_agent = request.headers.get('User-Agent')
-    return render_template('index.html', current_date=datetime.utcnow())
+    return render_template('index.html', current_date=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
